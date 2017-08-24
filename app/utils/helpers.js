1a625b808e488d836905d554118f8de936c2882d
Alex Tam <AJT@dhcp-v085-001.mobile.uci.edu>
2017-08-23 20:30:47 -0700

    utilities helper routes v1

---
+++ app/utils/helpers.js
@@ -0,0 +1,22 @@
+// REQUIRE AXIOS LIBRARY - HTTP REQUESTS
+var axios = require('axios');
+
+// STORE API KEY
+var APIKey = '3cb0f1e8f11a41038e94d076cf48791b';
+
+// HELPER FUNCTIONS
+var helpers = {
+    
+    // RUN QUERY TO TAKE INPUT DATA FROM EXECUTION
+    runQuery: function()
+
+}
+
+// REQUIRED SCHEMAS
+var User = require('../models/trade');
+var Trade = require('../models/user');
+
+// GET USER BALANCE
+
+
+// post route
